a94a7f862ec45f0edb0989c35091d374f3759e09
Crab <459062059@qq.com>
2019-05-06 14:29:29 +0800

    Text demo

--- lib/example/simple/basewidgets/TextDemo.dart
+++ lib/example/simple/basewidgets/TextDemo.dart
@@ -4,6 +4,16 @@ import 'package:flutter/material.dart';
  * Description:
  * Author:          peixuan.xie
  * Time:            2019-04-30 16:43
+ *
+ */
+
+/**
+ *  textAlign:文本的对齐方式: 可以选择左对齐、右对齐还是居中。注意，对齐的参考系是Text widget本身。本例中虽然是指定了居中对齐，但因为Text文本内容宽度不足一行，Text的宽度和文本内容长度相等，那么这时指定对齐方式是没有意义的，只有Text宽度大于文本内容长度时指定此属性才有意义。
+ *
+ *  maxLines、overflow: 指定文本显示的最大行数，默认情况下，文本是自动折行的，如果指定此参数，则文本最多不会超过指定的行。如果有多余的文本，可以通过overflow来指定截断方式，默认是直接截断，本例中指定的截断方式TextOverflow.ellipsis，它会将多余文本截断后以省略符“...”表示；TextOverflow的其它截断方式请参考SDK文档。
+ *
+ *  textScaleFactor: 代表文本相对于当前字体大小的缩放因子，相对于去设置文本的样式style属性的fontSize，它是调整字体大小的一个快捷方式。该属性的默认值可以通过MediaQueryData.textScaleFactor获得，如果没有MediaQuery，那么会默认值将为1.0。
+ *
  */
 
 class TextDemo extends StatelessWidget {
@@ -30,6 +40,46 @@ class TextDemo extends StatelessWidget {
             Text(
               "Hello world",
               textScaleFactor: 1.5,
+            ),
+            Text(
+              "Hello World",
+              textScaleFactor: 2,
+            ),
+            Text(
+              "Hello world",
+              style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 18.0,
+                  height: 1.2,
+                  fontFamily: 'Courier',
+                  background: new Paint()..color = Colors.yellow,
+                  decoration: TextDecoration.underline,
+                  decorationStyle: TextDecorationStyle.dashed),
+            ),
+            Text.rich(TextSpan(children: [
+              TextSpan(text: "Home:"),
+              TextSpan(
+                text: "http://flutterchina.club",
+                style: TextStyle(color: Colors.blue),
+              )
+            ])),
+            DefaultTextStyle(
+              style: TextStyle(color: Colors.red, fontSize: 20.0),
+              textAlign: TextAlign.start,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("测试子树继承widget树的样式"),
+                  Text("测试子树继承widget树的样式"),
+                  Text(
+                    "测试子树继承widget树的样式",
+                    style: TextStyle(
+                        inherit: false, //不继承默认样式
+                        color: Colors.grey
+                    ),
+                  )
+                ],
+              ),
             )
           ],
           crossAxisAlignment: CrossAxisAlignment.start,
